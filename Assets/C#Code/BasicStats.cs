65cba66967bc10497f3bf1587d78c2b67748c0cf
AlexandruNutu <alexander200950@gmail.com>
2017-03-13 21:42:18 +0200

    First commit

---
+++ Assets/C#Code/BasicStats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BasicStats {
+
+	public string UserClass;
+	public float baseAttackPower;
+	public float curAttackPower;
+	public float baseAttackSpeed;
+	public float curAttackSpeed;
+	public float baseDodge;
+	public float curDodge;
+	public float baseHitPercent;
+	public float curHitPercent;
+
+}
---
+++ Assets/C#Code/UserStats.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserStats : MonoBehaviour {
+
+	public string username; // numele jucatorului
+	public int level;//
+	public string UserClass;// pentru a sti in ce clasa este user-ul
+
+	public float curHp;//float pt o experienta smoothly
+	public float maxHp;
+	public float curMana;
+	public float maxMana;
+
+	public float baseAttackPower;// pentru atacul inamicilor
+	public float curAttackPower;
+	public float baseAttackSpeed;
+	public float curAttackSpeed;
+	public float baseDodge;//poate baram atacurile inamicilor
+	public float curDodge;
+	public float baseHitPercent;// trebuie sa avem un procent anume
+	public float curHitPercent;
+
+	public float hpRegenTimer;
+	public float hpRegenAmount;
+	public float manaRegenTimer;
+	public float manaRegenAmount;
+
+	public float curXp;
+	public float maxXp;
+
+	public bool isDead;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
